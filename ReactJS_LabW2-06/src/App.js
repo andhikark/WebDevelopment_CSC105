548d0887dc4cf31ce935ad168fd2f9d12274b060
Andhika_RK <75937835+andhikark@users.noreply.github.com>
2023-03-27 16:55:46 +0700

    Add files via upload

---
+++ ReactJS_LabW2-06/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import TodoPage from "./Page/TodoPage";
+function App(){
+  return(
+    <TodoPage/>
+  );
+}
+export default App;
---
+++ ReactJS_LabW2-06/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/andhika.jpg" alt="profile" />
+            <h1 className="name">Andhika Restu</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
